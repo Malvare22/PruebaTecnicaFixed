bb9508c0a14de9aac7280c0f6fecde9d8084baa7
Rodrigo Malaver <pingoalex22@hotmail.com>
2024-08-13 17:40:46 -0500

    Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
+import ProductModal from './components/modal/ProductModal'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <ProductModal></ProductModal>
       <Footer></Footer>
     </>
   )
---
+++ src/components/modal/ProductModal.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Button, Modal } from 'antd';
+import { CSSProperties } from 'react';
+import Man from '../../assets/img/buying.png';
+import './Modal.css'
+import { Check } from '../../assets/svg/Check';
+
+export default function ProductModal(){
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal className='modalStyle' closeIcon={false} open={isModalOpen} onOk={handleOk} footer={null} onCancel={handleCancel}>
+        
+        <img src={Man} width={'520px'}></img>
+        <div style={{display: 'flex', justifyContent:'center', margin: '20px'}}>
+          <div>
+            <img src={Man} width={'120px'} height={'140px'}></img>
+          </div>
+          <div style={{paddingLeft: '20px'}}>
+            <div style={{color: '#004AC1', fontSize: '16px', fontWeight:'bold', textAlign:'end'}}>$3,499.00 x 1</div>
+            <div style={{color: '#2B3445', fontSize: '16px', fontWeight:'bold', width:'60%'}}>Samsung A10S 13+2MP 64GB, 4GB RAM</div>
+            <div style={{color: '#7D879C', fontSize: '14px', fontWeight:'bold'}}>Color seleccionado: Gris Ratón</div>
+          </div>
+        </div>
+        <div style={{display: 'flex', justifyContent:'space-between', alignContent:'center', padding:'18px 40px 18px 40px', borderTop:'1px #707070 solid', borderBottom:'1px #707070 solid'}}>
+          <div style={{color: '#7D879C', fontSize:'16px', fontWeight:'bolder'}}>1 ítem en tu carrito</div>
+          <div style={{display: 'flex', alignContent: 'center'}}>
+            <div style={{color: '#7D879C', fontSize:'16px', fontWeight:'bolder'}}>Subtotal</div>
+            <div style={{color: '#004AC1', fontSize:'16px', fontWeight:'bolder', marginLeft:'10px'}}>$3.499,00</div>
+          </div>
+        </div>
+        <div className='rows'>
+          <div style={{display: 'flex' , justifyContent: 'center', alignContent:'center', alignItems: 'center', marginBottom: '10px'}}>
+            <div style={{width: '70px', height: '70px', display: 'flex', justifyContent: 'center', alignContent:'center', alignItems:'center', boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', borderRadius:'50%'}}><div className={'check'}><Check></Check></div></div>
+          </div>
+          <div style={{color:'#2B3445', fontWeight: 'bold', fontSize:'14px'}}>Te vas a llevar este celular por solo</div>
+          <div style={{color:'#2B3445', fontWeight: 'bold', fontSize:'20px'}}>$120 p/semana!</div>
+          <div style={{fontWeight: 'bold', fontSize:'20px'}}><Button 
+          style={{margin: '14px', backgroundColor: '#FFD300', color: '#004AC1', fontSize:'16px', fontWeight: 'bold', height: '56px', width: '220px'}
+        }>COMPRAR A CRÉDITO</Button></div>
+          <div style={{color:'#2B3445', fontStyle:'italic', fontSize:'12px'}}>o puedes</div>
+          <div style={{color:'#004AC1', fontWeight: 'bold', fontSize:'14px'}}>Comprar a contado</div>
+        </div>
+      </Modal>
+    </>
+  );
+};
