70ffff5de17778749260add314136cc9c8d9a834
Rodrigo Malaver <pingoalex22@hotmail.com>
2024-08-21 15:51:35 -0500

    Fix card

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Checkbox, Input, InputNumber } from "antd"
+import { Button, Card, Checkbox, Col, Input, InputNumber, Row } from "antd"
 import './ProductCard.css'
 import ReviewsScore from "../reviews/ReviewsScore"
 import Like, { LikeFrame } from "../like/Like"
@@ -13,7 +13,7 @@ interface ProductCardProps{
   product: Product;
 }
 
-export const priceStyle : React.CSSProperties = {fontSize: '22px', color: '#004AC1', fontWeight: 'bolder'};
+export const priceStyle : React.CSSProperties = {fontSize: '28px', color: '#004AC1', fontWeight: 'bolder'};
 
 export const oldPriceStyle : React.CSSProperties = {fontSize: '16px', marginTop: '-10px', color: '#7D879C', fontWeight: 'bolder', textAlign:'end', textDecorationLine: 'line-through'};
 
@@ -29,25 +29,32 @@ const ProductCard:  React.FC<ProductCardProps> = ({ product }) => {
   return (
     <>
         <Card style={{ width: '100%', height: 480, borderRadius: '20px' }} className="productCard">
-            <div style={{display: 'flex', justifyContent: 'center', padding: 10}}>
+            <Row style={{display: 'flex', justifyContent: 'center', padding: 10}}>
               <LikeFrame product={product} height="300px"></LikeFrame>           
-            </div>
-            <div style={{padding: '0px 16px 10px 16px'}}>
-              <div style={{display:'flex', justifyContent: 'space-between'}}>
-                <div>
-                  <Link to={'/product?id=' + product.id} style={{fontWeight: 'bold', fontSize: '12px', marginTop: '10px', marginBottom: '4px'}}>{product.name}</Link>
-                  <div><ReviewsScore score={getScore(product.reviews)}/></div>
-                </div>
-                <div>
-                  <div style={priceStyle}>${getRealValue(product)}</div>
-                  <div style={oldPriceStyle}>${product.price}</div>
-                </div>
-              </div>
-              <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '10px'}}>
-                <div style={{color: '#7D879C', fontSize: '10px'}}>$120 p/semana <br></br> o $520 p/mes</div>
-                <Button className={'yellowButton'} onClick={handleButton}>Lo Quiero!</Button>
-              </div>
-            </div>
+            </Row>
+            <Row style={{padding: '0px 16px 10px 16px', height: '170px'}} align={'stretch'}>
+              <Col span={12} style={{height: '100%'}}>
+                <Row style={{height: '90%', display:'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                  <Row>
+                    <Link to={'/product?id=' + product.id} style={{fontWeight: 'bold', fontSize: '12px', marginTop: '10px', marginBottom: '4px'}}>{product.name}</Link>
+                    <ReviewsScore score={getScore(product.reviews)}/></Row>
+                    <Row>
+                      <div style={{color: '#7D879C', fontSize: '10px'}}>$120 p/semana <br></br> o $520 p/mes</div>
+                    </Row>
+                  </Row>
+              </Col>
+              <Col span={12}>
+                <Row style={{height: '90%', display:'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                    <Row justify={'end'} style={{marginTop: '2px'}}>
+                      <div style={priceStyle}>${getRealValue(product)}</div>
+                      <div style={oldPriceStyle}>${product.price}</div>
+                    </Row>
+             
+                    <Row justify={'end'}><Button className={'yellowButton'} onClick={handleButton}>Lo Quiero!</Button></Row>
+                  
+                </Row>
+              </Col>
+            </Row>
         </Card>
     </>
   )
